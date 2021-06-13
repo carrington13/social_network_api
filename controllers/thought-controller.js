e1c0ab4d6b72a51887966717b3c2a74476b6d101
Mediogre <caseyarrington13@gmail.com>
2021-06-13 18:42:03 -0500

    initial setup and file-staging

---
+++ controllers/thought-controller.js
@@ -0,0 +1,17 @@
+// Route /api/thoughts
+
+//Get all thoughts
+
+// Get one thought by _id
+
+// Create a new thought and push its id to users thoughts array field
+
+// Update a thought by _id
+
+// Delete a thought by _id
+
+// Route /api/thoughts/:thoughtId/reactions
+
+// Create a reaction stored in a single thoughts reactions array field
+
+// Delete to pull and remove a reaction by the reactions reactionId value
---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+// Routes = /api/users
+
+// Get all users
+
+// Get a single user by _id
+
+// Add A User
+
+// update a user by _id
+
+// Delete a user by _id
+
+
+// Bonus: Remove a user's associated thoughts when deleted.
