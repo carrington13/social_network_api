f4aab247a7efb26a4fa862b94241b067b13b9423
Mediogre <caseyarrington13@gmail.com>
2021-06-13 21:28:17 -0500

    add User, Thought and index in models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { truncate } = require('fs');
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            require: 'Username is require!',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        // thoughts [_id values] referencing Thought model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        // Array of _id values referencing the User model (self- reference)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
